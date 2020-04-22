42d9e76aa14ff07a28ab88d07785dc12e3a1173e
Mike V <mvee333@gmail.com>
2020-04-22 13:01:12 -0400

    initial commit with starting js

---
+++ assets/javascript/script.js
@@ -0,0 +1,38 @@
+// Default prompt containing Coding Quiz Challenge, descripton and start button
+// When I click the start button, then the timer starts and the first question is displayed
+let startButton = document.getElementById("start-btn");
+let questionContainerElement = document.getElementById("question-container");
+let questionsEle, emt = document.getElementById("question");
+let answerButtonsElement = document.getElementById("answer-buttons");
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    console.log("started");
+    startButton.classList.add("hide")
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion(currentQuestionIndex)
+}
+
+function showQuestion(question) {
+    questionContainerElement.innerText = question.question
+    question.answers.forEach
+}
+
+function selectAnswer() {
+
+}
+
+let questions = [
+    {
+        question: "what is 2+2",
+        answers: [
+            { text: "4", correct: true },
+            { text: "22", correct: false }
+        ]
+    }
+]
